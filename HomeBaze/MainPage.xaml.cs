759fd62de3bdeceeaee00632e3b6d3fa57a51798
dwe <dwe@WS09>
2022-05-30 13:29:49 +0200

    Projektdateien hinzufügen.

---
+++ HomeBaze/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HomeBaze
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var button = new Button()
+            {
+                Text = "Click me."
+            };
+            button.Clicked += OnClick;
+            void OnClick(object sender, EventArgs e) { Console.WriteLine("lol button has been pressed"); };
+
+            InitializeComponent();
+        }
+    }
+}
